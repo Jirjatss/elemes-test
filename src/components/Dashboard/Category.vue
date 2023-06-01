16e4d7cb97917e7ccfa479a4b5a0bfa94e40804f
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2023-06-01 12:23:54 +0800

    install swiper

--- src/components/Dashboard/Category.vue
+++ src/components/Dashboard/Category.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script>
 import CardCategory from "../Card/CardCategory1.vue";
 import CardCategory2 from "../Card/CardCategory2.vue";
 import Cupcake from "../../assets/image/Cupcake.png";
@@ -6,19 +6,68 @@ import Pizaa from "../../assets/image/pizza.png";
 import Kebab from "../../assets/image/kebab.png";
 import Salmon from "../../assets/image/salmon.png";
 import Donat from "../../assets/image/doughnut.png";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+    CardCategory,
+    CardCategory2,
+  },
+  setup() {
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log("slide change");
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+      modules: [Navigation, Pagination, Scrollbar, A11y],
+    };
+  },
+};
 </script>
 
 <template>
   <div class="bg-white md:px-16 px-8 md:-mt-20">
     <h1 class="md:text-[38px] text-[24px] text-black">Browser Our Category</h1>
     <h1 class="md:text-[38px] text-[24px] text-[#8BAC3E]">Receipt</h1>
-    <div class="grid md:grid-cols-5 grid-cols-1 justify-center gap-2 gap-y-2 mt-10" data-aos="fade-up">
+    <swiper :modules="modules" :slides-per-view="3" :space-between="50" navigation @swiper="onSwiper" @slideChange="onSlideChange">
+      <swiper-slide>
+        <CardCategory product="Cupcake" jumlah="22 Items" :image="Cupcake" backgroundColor="#F0FEEB" />
+      </swiper-slide>
+      <swiper-slide>
+        <CardCategory2 product="Pizza" jumlah="25 Items" :image="Pizaa" backgroundColor="#e1eff0" />
+      </swiper-slide>
+      <swiper-slide>
+        <CardCategory product="Kebab" jumlah="12 Items" :image="Kebab" backgroundColor="#EAEEFA" />
+      </swiper-slide>
+      <swiper-slide>
+        <CardCategory2 product="Salmon" jumlah="22 Items" :image="Salmon" backgroundColor="#F9EEF3" />
+      </swiper-slide>
+      <swiper-slide>
+        <CardCategory product="Doughnut" jumlah="11 Items" :image="Donat" backgroundColor="#F3F7D9" />
+      </swiper-slide>
+      ...
+    </swiper>
+    <!-- <div class="grid md:grid-cols-5 grid-cols-1 justify-center gap-2 gap-y-2 mt-10" data-aos="fade-up">
       <CardCategory product="Cupcake" jumlah="22 Items" :image="Cupcake" backgroundColor="#F0FEEB" />
       <CardCategory2 product="Pizza" jumlah="25 Items" :image="Pizaa" backgroundColor="#e1eff0" />
       <CardCategory product="Kebab" jumlah="12 Items" :image="Kebab" backgroundColor="#EAEEFA" />
       <CardCategory2 product="Salmon" jumlah="22 Items" :image="Salmon" backgroundColor="#F9EEF3" />
       <CardCategory product="Doughnut" jumlah="11 Items" :image="Donat" backgroundColor="#F3F7D9" />
-    </div>
+    </div> -->
     <div class="flex md:justify-end justify-center w-full py-2 gap-2 px-8 mt-5">
       <button class="bg-[#8BAC3E] px-3 py-2 rounded-full flex gap-2">
         <img src="../../assets/image/prev.png" alt="" class="md:w-[33px] w-[15px] m-auto" />
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueAwesomeSwiper from "../node_modules/swiper";
 import "./assets/style.css";
-import VueSlickCarousel from "vue-slick-carousel";
-import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "swiper/swiper-bundle.css";
 
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
-app.component("vue-slick-carousel", VueSlickCarousel);
+app.use(VueAwesomeSwiper);
